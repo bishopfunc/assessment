cf05d4a3d131db4e74e91f91af3cc424d3b5bf3e
bishopfunc <61902652+bishopfunc@users.noreply.github.com>
2020-05-31 10:45:11 +0900

    #1に対する対応をしました

--- assessment.js
+++ assessment.js
@@ -50,9 +50,9 @@ assessmentButton.onclick = () => {
 };
 
 const answers = [
-  '{userName}のいいところは声です。{userName}の特徴的な声はみなを惹きつけ、心に残ります。',
   '{userName}のいいところはまなざしです。{userName}に見つめられた人は、気になって仕方がないでしょう。',
   '{userName}のいいところは情熱です。{userName}の情熱に周りの人は感化されます。',
+  '{userName}のいいところは声です。{userName}の特徴的な声はみなを惹きつけ、心に残ります。',
   '{userName}のいいところは厳しさです。{userName}の厳しさがものごとをいつも成功に導きます。',
   '{userName}のいいところは知識です。博識な{userName}を多くの人が頼りにしています。',
   '{userName}のいいところはユニークさです。{userName}だけのその特徴が皆を楽しくさせます。',
